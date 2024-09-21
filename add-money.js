582b5be36ec093a9627b447f0af4d9a6c576a23e
Foysal107 <foysalmiah8443@gmail.com>
2024-09-21 20:21:49 +0600

    add money section done

--- add-money.js
+++ add-money.js
@@ -3,8 +3,17 @@ document.getElementById('btn-add-money')
     event.preventDefault();
   
 const addMoney =document.getElementById('input-add-money').value;
-
+const addMoneyNumber = parseFloat(addMoney)
 const addPassword = document.getElementById('input-pin-number').value;
 console.log(addPassword,addMoney);
-
+// wrong way to verify pin  
+if(addPassword==='1234'){
+const blance = document.getElementById('Account').innerText;
+const blanceNumber = parseFloat(blance);
+const newblance = blanceNumber + addMoneyNumber;
+document.getElementById('Account').innerText=newblance;
+}
+else{
+    alert('Faild to add money')
+}
 })
