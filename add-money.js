c20d6f08239cab2ea85df2ff2619c48ad25cff95
Foysal107 <foysalmiah8443@gmail.com>
2024-09-21 19:51:55 +0600

    input fild

---
+++ add-money.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-add-money')
+.addEventListener('click',function(event){
+    event.preventDefault();
+  
+const addMoney =document.getElementById('input-add-money').value;
+
+const addPassword = document.getElementById('input-pin-number').value;
+console.log(addPassword,addMoney);
+
+})
